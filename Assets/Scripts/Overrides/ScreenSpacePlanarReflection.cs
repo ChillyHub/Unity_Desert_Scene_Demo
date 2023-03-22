f1b58cfe2799739990a1349993fe715bb4d04d99
Ben Chilly <1533935729@qq.com>
2023-03-23 05:27:32 +0800

    2023/3/23 5:27

---
+++ Assets/Scripts/Overrides/ScreenSpacePlanarReflection.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomRenderer
+{
+    public class ScreenSpacePlanarReflection
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RendererFeature/ScreenSpacePlanarReflectionRendererFeature.cs
@@ -0,0 +1,7 @@
+﻿namespace RendererFeature
+{
+    public class ScreenSpacePlanarReflectionRendererFeature
+    {
+        
+    }
+}
